f0609c5601f1a6db62f97a3494ce730db1ff00d6
IAPONCEV <157499186+IanPonceV@users.noreply.github.com>
2024-02-27 09:23:51 -0600

    Add files via upload

---
+++ Semana 7/Laboratoriopc_01.cs
@@ -0,0 +1,31 @@
+﻿namespace Laboratoriopc_01
+{
+    class Laboratoriopc_01
+    {
+        static void Main(string[] args) 
+        {
+            string nombre;
+            string edad;
+            string carnet;
+            string carrera;
+
+            Console.WriteLine("Ingrese su nombre");
+            nombre=Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad");
+            edad=Console.ReadLine();
+
+            Console.WriteLine("Ingrese su carnet");
+            carnet=Console.ReadLine();
+
+            Console.WriteLine("Ingrese su carrera");
+            carrera=Console.ReadLine();
+
+            Console.WriteLine("Soy"+nombre+"tengo"+edad+"estudio la carrera de"+carrera+"mi carnet es"+carnet);
+            Console.ReadKey();
+        
+
+        }
+    }
+ 
+}
